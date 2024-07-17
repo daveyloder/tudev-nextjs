8b0d63018c26a5f3ccd07411e2bda6673ffe44bd
daveyloder <davidloder92@gmail.com>
2024-07-17 13:50:09 -0400

    commit for stuff already done

---
+++ app/not-found.js
@@ -0,0 +1,36 @@
+"use client";
+
+import React from "react";
+
+import Link from "next/link";
+import { Container, Row, Col } from "reactstrap";
+
+export default function NotFound() {
+  return (
+    <>
+      {/* Site Header */}
+
+      <header
+        style={{
+          height: "400px",
+        }}
+      >
+        <Container className="pt-5 mb-4 text-white">
+          <Row>
+            <Col>
+              <h1 className="display-1">404</h1>
+              <h4 className="display-4">This page is not found</h4>
+              <p>
+                You should probably click <Link href="/">here</Link> to go back
+                to home. Do not click links on the navbar for the love of god!
+              </p>
+            </Col>
+          </Row>
+          <Row> </Row>
+        </Container>
+      </header>
+
+      {/* Site Header */}
+    </>
+  );
+}
