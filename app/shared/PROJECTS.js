cda2ffc491642d971073a798d9f4dbe7ad1f2269
daveyloder <davidloder92@gmail.com>
2024-07-20 15:00:21 -0400

    Added real projects to the Links file under shared

--- app/shared/PROJECTS.js
+++ app/shared/PROJECTS.js
@@ -1,29 +1,37 @@
 export const PROJECTS = [
-  //   {
-  //     id: 1,
-  //     projectTitle: "Why You Need A NAS",
-  //     projectDescription:
-  //       "A short project on why you need a home Network Accessed Server demostrated using TrueNas Scale",
-  //     projectAuthor: "David Loder",
-  //     projectLink: "",
-  //     featuredProject: true,
-  //   },
-  //   {
-  //     id: 2,
-  //     projectTitle: "I made a Black Hole",
-  //     projectDescription:
-  //       "How I taught myself to render a simulation of a black hole on my computer",
-  //     projectAuthor: "John Doe",
-  //     projectLink: "/",
-  //     featuredProject: true,
-  //   },
-  //   {
-  //     id: 3,
-  //     projectTitle: "You need a website",
-  //     projectDescription:
-  //       "Why you need a website, using Avata a WordPress plugin",
-  //     projectAuthor: "John Doe",
-  //     projectLink: "/",
-  //     featuredProject: true,
-  //   },
+  {
+    id: 1,
+    projectTitle: "Why you need a home NAS. ",
+    projectDescription:
+      "A short project on why you need a home Network Accessed Server demostrated using TrueNas Scale",
+    projectAuthor: "David Loder",
+    projectLink: "",
+    featuredProject: true,
+  },
+  {
+    id: 2,
+    projectTitle: "Playing Minecraft with your Hands",
+    projectDescription:
+      "How to create gesture interface for your webcam to play Minecraft",
+    projectAuthor: "Jayming Liu",
+    projectLink: "/",
+    featuredProject: true,
+  },
+  {
+    id: 3,
+    projectTitle: "Wordpress and Why You Should Use It",
+    projectDescription:
+      "Why you need a website, using Avata a WordPress plugin",
+    projectAuthor: "Josh O'Halloran",
+    projectLink: "https://joshuaohalloran.me/",
+    featuredProject: true,
+  },
+  {
+    id: 4,
+    projectTitle: "CookBook Pro",
+    projectDescription: "A Receipe and Meal Planner App build on ReactJS",
+    projectAuthor: "Shawn Dutill",
+    projectLink: "https://cookbook-pro-49b95.firebaseapp.com/",
+    featuredProject: true,
+  },
 ];
