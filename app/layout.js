bbab778ac6fcf41d01ce01c24d8d0363515e9e27
daveyloder <davidloder92@gmail.com>
2024-09-06 18:24:51 -0400

    Updated website metadata

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,19 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { Inter } from "next/font/google";
 import BootstrapClient from "@/app/_components/BootstrapClient";
 import "./globals.css";
 import NavbarComponent from "@/app/_components/navbars/NavbarComponent";
 import FooterComponent from "@/app/_components/footers/FooterComponent";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
   title: "TUDev",
-  description: "Temple University's student-run coding organization",
+  description: "Temple University's community of hackers and makers.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <NavbarComponent />
         {children}
         <FooterComponent />
